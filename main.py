a7ee373d7174bc427b8bbc770c999f187d30ead9
skian652 <69122934+skian652@users.noreply.github.com>
2024-02-23 21:33:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,28 +25,28 @@ Config = {
     "Mines" : { # Config for mines
         "House" : 0.1,  # The Multiplier Will Be Multiplied by 1.00 - This
     },
-    "Logs": 1200137132593926184, # log channel
+    "Logs": 1208840607607291964, # log channel
     "Crash" : { # Config for crash
         "InstaCrashChance" : 10,  # Chance That It Will Crash at 1.00x
         "CrashChance" : 2,  # The Lower This Number Is The Higher Your Multipliers Will Average, I find 2 is the best
-        "ChannelID" : "1200138115902353589"  # Id of the channel crash games will be in
+        "ChannelID" : "1210775924807704596"  # Id of the channel crash games will be in
     },
     "Coinflip" : { # Config for coinflip
-        "1v1" : "1200137132593926184",  # Channel That Coinflips Be In
+        "1v1" : "1208840607850565650",  # Channel That Coinflips Be In
         "House": 3.5 # House Edge (%)
     },
     "Rains" : { # Config for rains
-        "Channel" : "1200137196766765221" # Set to the id the channel rains will be in
+        "Channel" : "1208840607850565651" # Set to the id the channel rains will be in
     },
     "AdminCommands" : {
-        "UserID" : ["1175461563554091009"] # if more than 1 do this: ["1st user id", "2nd user id"]
+        "UserID" : ["1208840607175413774"] # if more than 1 do this: ["1st user id", "2nd user id"]
     },
     "Upgrader": { # Config for upgrader
         "House": 0.95 # house edge (winnings*house)
     },
     "Rakeback" : 1, # Rakeback %
-    "Username": "blinxostock", # The Username Of The Account Running The Bot
-    "DiscordBotToken": "MTIwMDEzODM0MTc1MzA0MDkwNg.GfNNEQ.W6ZCflzXBqOmY1AWwTStyLa7RCJnWiCSNmVo-Y" # The token of the discord bot
+    "Username": "Wxrewxlf_YT", # The Username Of The Account Running The Bot
+    "DiscordBotToken": "MTIxMDc3NjM0ODkwNjIzMzg4Nw.GmX2jV.s2rNB5--ibJ1qKROU_z-YMnyIgSBwFVpJZW9ng" # The token of the discord bot
 }
 username = Config['Username']
 
